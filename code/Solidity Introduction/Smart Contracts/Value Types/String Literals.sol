33b7c930da0402341332fb9d0567a061778f9ed7
kalkidan zeleke <kalkidanweldeyes@gmail.com>
2026-05-05 08:20:07 -0700

    Add files via upload
    Add smart contract implementation

---
+++ code/Solidity Introduction/Smart Contracts/Value Types/String Literals.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    bytes32 public msg1 = "Hello World";
+    string public msg2 = "This is a very long string that exceeds thirty two bytes in length for sure!";
+}
---
+++ code/Solidity Introduction/Smart Contracts/Value Types/Unsigned Integers.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.20;
+
+contract Contract {
+        uint8 public a = 100;
+    uint16 public b = 256;
+    uint256 public sum = a + b;
+    
+}
