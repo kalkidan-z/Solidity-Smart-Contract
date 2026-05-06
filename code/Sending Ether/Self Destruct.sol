0b190985629b71a6c2645c6b92fed2210f52c205
Yeabsra-Getahun <becherinetu@gmail.com>
2026-05-06 11:05:47 +0300

    Add:reverting transcation and sending ether

---
+++ code/Sending Ether/Self Destruct.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    address public owner;
+    address public charity;
+    
+    constructor(address _charity) {
+        owner = msg.sender;
+        charity = _charity;
+    }
+    
+    receive() external payable {
+        // This function allows the contract to receive Ether
+    }
+    
+    function tip() public payable {
+        payable(owner).transfer(msg.value);
+    }
+    
+    function donate() public {
+        payable(charity).transfer(address(this).balance);
+        selfdestruct(payable(charity));
+    }
+}
